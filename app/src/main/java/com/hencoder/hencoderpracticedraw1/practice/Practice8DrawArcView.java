1fa139ce9eebc4fc6e1e63fc74ae1594b33fd2da
liguoying <alinemklee@yeah.net>
2017-08-30 17:55:47 +0800

    draw arc

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
+
+    private Paint mPaint = new Paint();
 
     public Practice8DrawArcView(Context context) {
         super(context);
@@ -23,6 +26,12 @@ public class Practice8DrawArcView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(200,100,800,500,-100,90,true,mPaint);
+        canvas.drawArc(200,100,800,500,20,100,false,mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(200,100,800,500,140,70,false,mPaint);
+
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
     }
