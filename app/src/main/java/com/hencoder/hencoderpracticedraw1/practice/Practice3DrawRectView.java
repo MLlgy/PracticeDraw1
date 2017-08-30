b36f108b6f6c32f2616ddac2af1cc639201e9d77
liguoying <alinemklee@yeah.net>
2017-08-30 16:42:44 +0800

    draw poit && rect

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,11 +2,20 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw1.R;
+
 public class Practice3DrawRectView extends View {
+    private Paint mPaint = new Paint();
+    private Rect mRect = new Rect(600,100,900,400);
+    private RectF mRectF = new RectF(600,450,900,750);
 
     public Practice3DrawRectView(Context context) {
         super(context);
@@ -23,7 +32,11 @@ public class Practice3DrawRectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(200,100,500,400,mPaint);
+        canvas.drawRect(mRect,mPaint);
+        canvas.drawRect(mRectF,mPaint);
 //        练习内容：使用 canvas.drawRect() 方法画矩形
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
@@ -2,11 +2,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice4DrawPointView extends View {
+
+    private Paint mPaint = new Paint();
+
 
     public Practice4DrawPointView(Context context) {
         super(context);
@@ -23,6 +27,11 @@ public class Practice4DrawPointView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mPaint.setStrokeWidth(50);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvas.drawPoint(50, 50, mPaint);
+        mPaint.setStrokeCap(Paint.Cap.SQUARE);
+
 
 //        练习内容：使用 canvas.drawPoint() 方法画点
 //        一个圆点，一个方点
